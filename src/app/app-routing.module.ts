7dccc2e5a6400e3409131ff20b3132dec0bfdc86
HenryMiyawaki <miyawakihenry@gmail.com>
2023-06-14 02:32:16 -0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AreasComponent } from './views/areas/areas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'areas',
+    component: AreasComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
